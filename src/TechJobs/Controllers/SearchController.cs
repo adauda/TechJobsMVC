d90d28df1eb1277241ecb3a1cd2262ac90453724
adauda <quadri_d1@yahoo.com>
2017-03-19 11:18:22 -0700

    TechJobsMVC done

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;           //line added by me
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using TechJobs.Models;
 
@@ -6,6 +7,9 @@ namespace TechJobs.Controllers
 {
     public class SearchController : Controller
     {
+        internal static Dictionary<string, string> columnChoices = new Dictionary<string, string>();
+     
+
         public IActionResult Index()
         {
             ViewBag.columns = ListController.columnChoices;
@@ -15,6 +19,31 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            ViewBag.columns = ListController.columnChoices;
+
+            List<Dictionary<String, String>> searchResults;
+
+            // Fetch results
+            if (searchType.Equals("all"))
+            {
+                
+                searchResults = JobData.FindByValue(searchTerm);
+             
+            }
+            else
+            {
+                searchResults = JobData.FindByColumnAndValue(searchType, searchTerm);
+               
+            }
+
+            //List<Dictionary<String, String>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            ViewBag.title = "Jobs with " + ListController.columnChoices[searchType] + ": " + searchTerm;
+            ViewBag.jobs = searchResults;
+
+            return View("Index");
+        }
 
     }
 }
