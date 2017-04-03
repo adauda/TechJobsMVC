1e907cbcddfe540b45ba813e46d206181362141d
adauda <quadri_d1@yahoo.com>
2017-04-03 06:17:18 -0700

    TechJobsMVC done

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -1,4 +1,64 @@
-﻿using System;           //line added by me
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TechJobs.Models;
+
+namespace TechJobs.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Search";
+            return View();
+        }
+
+        // TODO #1 - Create a Results action method to process 
+        // search request and display results
+
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Search";
+            //look up search results via JobData class   
+            //if user searches 'all'
+            /*
+            if (searchType == "all")
+            {
+                ViewBag.Jobs = JobData.FindByValue(searchTerm);
+            }
+            */
+            if (searchType.Equals("all"))
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindAll();
+                ViewBag.title = "All Jobs";
+                ViewBag.jobs = jobs;
+                //return View("Jobs");
+            }
+
+            //if user searches by category
+            else
+            {
+                ViewBag.Jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+            //pass them into Views/Search/index.cshtml 
+
+            return View("~/Views/Search/Index.cshtml");
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using TechJobs.Models;
@@ -7,7 +67,7 @@ namespace TechJobs.Controllers
 {
     public class SearchController : Controller
     {
-        internal static Dictionary<string, string> columnChoices = new Dictionary<string, string>();
+        //internal static Dictionary<string, string> columnChoices = new Dictionary<string, string>();
      
 
         public IActionResult Index()
@@ -22,15 +82,31 @@ namespace TechJobs.Controllers
         public IActionResult Results(string searchType, string searchTerm)
         {
             ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Search";
 
-            List<Dictionary<String, String>> searchResults;
+            //look up search results via JobData class
+            //if user searches 'all'
+            if ( searchType == "all")
+            {
+                ViewBag.Jobs = JobData.FindByValue(searchTerm);
+            }
+            //if user searches by category
+            else
+            {
+                ViewBag.Jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+
+            // List<Dictionary<String, String>> searchResults;
 
             // Fetch results
+            /*
             if (searchType.Equals("all"))
             {
-                
-                searchResults = JobData.FindByValue(searchTerm);
-             
+                //searchResults = JobData.FindByValue(searchTerm);
+                //searchResults = JobData.FindAll();
+                ViewBag.Jobs =  = JobData.FindByValue(searchTerm);
+
+
             }
             else
             {
@@ -41,9 +117,15 @@ namespace TechJobs.Controllers
             //List<Dictionary<String, String>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
             ViewBag.title = "Jobs with " + ListController.columnChoices[searchType] + ": " + searchTerm;
             ViewBag.jobs = searchResults;
+            
 
+            //pass them into Views/Search/index.cshtml
             return View("Index");
+           // return View(" ~/Views/Search/Index.cshtml");
         }
 
     }
 }
+
+
+*/
